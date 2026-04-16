298c4d693318f3489033a207fd2d18e6efa094c9
ddsbusinessproducts <ddsbusinessproducts@gmail.com>
2026-04-16 15:36:09 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,8 @@ from discord.ext import commands
 from discord import app_commands
 
 import os
-TOKEN = os.getenv("TOKEN")STAFF_ROLE_NAME = "Staff"
+TOKEN = os.getenv("TOKEN")
+STAFF_ROLE_NAME = "Staff"
 
 intents = discord.Intents.default()
 intents.message_content = True
